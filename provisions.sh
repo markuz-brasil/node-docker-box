460e0c0c3bb23d71a188a660d464ed91536b42e2
Markuz GJ <markuz.br@gmail.com>
2015-03-27 19:29:56 -0700

    fix readme

--- provisions.sh
+++ provisions.sh
@@ -7,10 +7,9 @@ apt-get install -y curl wget libssl-dev git-core build-essential vim-nox zsh || 
 # [ -d "/usr/local/lib/node_modules" ] || {
   cd /root
   git clone --depth=1 "https://github.com/tj/n.git" /root/n &&
-  cd n && make install && n io use 1.6.2 && cd /root &&
+  cd n && make install && n io 1.6.2 && cd /root &&
   rm -rf /root/n || echo 'nodejs install failed'
   # using stable nodejs, and installing basic global pkgs.
-  n stable &&
   npm config set prefix="/usr/local" &&
   npm install -g pm2 yo bower gulp grunt-cli &&
   cd $BASE_PATH || echo 'npm install failed'
